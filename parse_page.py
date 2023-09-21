a6b029846259c855ab9dadcceb8d06bfefda2534
azazello15 <102389631+azazello15@users.noreply.github.com>
2023-09-21 18:24:50 +0300

    Update parse_page.py

--- parse_page.py
+++ parse_page.py
@@ -4,39 +4,87 @@ import time
 import undetected_chromedriver as uc
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from undetected_chromedriver import ChromeOptions
+
+options = Options()
+options.add_argument('--headless')
+driver = uc.Chrome(version_main=117, options=options)
+data = []
+try:
+    driver.get(
+        'https://www.tripadvisor.com/Restaurant_Review-g187482-d23943324-Reviews-Quebracho_Asador_Argentino-Santa_Cruz_de_Tenerife_Tenerife_Canary_Islands.html')
+    name = driver.find_element(By.CSS_SELECTOR, "[class='HjBfq']").text
+    open_now = driver.find_element(By.CSS_SELECTOR, "[class='NehmB']").text
+    '''details'''
+    details = driver.find_elements(By.CSS_SELECTOR, "[class='SrqKb']")
+    price_range = details[0].text
+    cusines = details[1].text
+    meals = details[2].text
+    '''details'''
+
+    '''location and contacts'''
+    locs = driver.find_elements(By.CSS_SELECTOR, "[class='kDZhm IdiaP']")
+    address = locs[0].text
+    location = locs[1].text
+
+    links = driver.find_elements(By.CSS_SELECTOR, "[class='YnKZo Ci Wc _S C FPPgD']")
+    geo = links[0].get_attribute('href')
+    website = links[1].get_attribute('href')
+    phones = driver.find_elements(By.CSS_SELECTOR, "[class='BMQDV _F Gv wSSLS SwZTJ']")
+    phone = phones[1].get_attribute('href')
+    '''location and cjntacts'''
+
+    '''Rating'''
+    rating_num = driver.find_element(By.CSS_SELECTOR, "[class='ZDEqb']").text
+    reviews_num = driver.find_element(By.CSS_SELECTOR, "[class='IcelI']").text
+
+    rating = driver.find_elements(By.CSS_SELECTOR, "[class='cNFlb']")
+    rating_1 = rating[0].text
+    rating_2 = rating[1].text
+    '''Rating'''
+    loc_links_dict = {
+        'Геолокация': geo,
+        'Вебсайт': website,
+        'Номер телефона': phone
+    }
+    location_dict = {
+        'Адрес': address,
+        'Местоположение': location,
+        'Способы связи': loc_links_dict
+    }
+    rating_dict = {
+        'Оценка': rating_num,
+        'Отзывы': reviews_num,
+        'Рейтинг_1': rating_1,
+        'Рейтинг_2': rating_2,
+    }
+    details = {
+        'Ценовой диапазон': price_range,
+        'КУХНИ': cusines,
+        'Еда': meals,
+        'Рейтинги и отзывы': rating_dict
+    }
+    data_dict = {
+        'Название': name,
+        'Расписание': open_now,
+        'Детали': details,
+        'Адреса и ссылки': location_dict
+    }
+    info = {
+            'INFO': data_dict
+    }
+    data.append(info)
+
+    with open('items.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+    time.sleep(3)
 
 
-def parse_links():
-    options = ChromeOptions()
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--headless')
-    driver = uc.Chrome(version_main=117, options=options)
-    with open('urls.txt', 'r') as f:
-        urls = f.read().split()
-
-    item_list = []
-    count = 0
-    for url in urls[1:]:
-        try:
-            driver.get(url)
-            links = driver.find_elements(By.CSS_SELECTOR, "[class='IdiaP ']")
-            maps = links[0].get_attribute('href')
-            website = links[1].get_attribute('href')
-            print(links)
-            # about = driver.find_element(By.CSS_SELECTOR, "[class='xHZAW']").text
-            # details = driver.find_elements(By.CSS_SELECTOR, "[class='ui_columns ']")
-            data = {
-                'Место расположения': maps,
-                'Вебсайт': website
-            }
-            item_list.append(data)
-        except Exception as ex:
-            print(f"Error occurred while parsing {url}: {str(ex)}")
-        finally:
-            driver.close()
-            driver.quit()
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
 
 
 if __name__ == '__main__':
