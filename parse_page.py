c1c8efe0fa906587889697e5fcda16fa1523f756
azazello15 <102389631+azazello15@users.noreply.github.com>
2023-09-24 19:57:34 +0300

    Update parse_page.py

--- parse_page.py
+++ parse_page.py
@@ -2,90 +2,134 @@ import json
 import time
 
 import undetected_chromedriver as uc
+from selenium.common import StaleElementReferenceException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 options = Options()
 options.add_argument('--headless')
-driver = uc.Chrome(version_main=117, options=options)
-data = []
-try:
-    driver.get(
-        'https://www.tripadvisor.com/Restaurant_Review-g187482-d23943324-Reviews-Quebracho_Asador_Argentino-Santa_Cruz_de_Tenerife_Tenerife_Canary_Islands.html')
-    name = driver.find_element(By.CSS_SELECTOR, "[class='HjBfq']").text
-    open_now = driver.find_element(By.CSS_SELECTOR, "[class='NehmB']").text
-    '''details'''
-    details = driver.find_elements(By.CSS_SELECTOR, "[class='SrqKb']")
-    price_range = details[0].text
-    cusines = details[1].text
-    meals = details[2].text
-    '''details'''
-
-    '''location and contacts'''
-    locs = driver.find_elements(By.CSS_SELECTOR, "[class='kDZhm IdiaP']")
-    address = locs[0].text
-    location = locs[1].text
-
-    links = driver.find_elements(By.CSS_SELECTOR, "[class='YnKZo Ci Wc _S C FPPgD']")
-    geo = links[0].get_attribute('href')
-    website = links[1].get_attribute('href')
-    phones = driver.find_elements(By.CSS_SELECTOR, "[class='BMQDV _F Gv wSSLS SwZTJ']")
-    phone = phones[1].get_attribute('href')
-    '''location and cjntacts'''
-
-    '''Rating'''
-    rating_num = driver.find_element(By.CSS_SELECTOR, "[class='ZDEqb']").text
-    reviews_num = driver.find_element(By.CSS_SELECTOR, "[class='IcelI']").text
-
-    rating = driver.find_elements(By.CSS_SELECTOR, "[class='cNFlb']")
-    rating_1 = rating[0].text
-    rating_2 = rating[1].text
-    '''Rating'''
-    loc_links_dict = {
-        'Геолокация': geo,
-        'Вебсайт': website,
-        'Номер телефона': phone
-    }
-    location_dict = {
-        'Адрес': address,
-        'Местоположение': location,
-        'Способы связи': loc_links_dict
-    }
-    rating_dict = {
-        'Оценка': rating_num,
-        'Отзывы': reviews_num,
-        'Рейтинг_1': rating_1,
-        'Рейтинг_2': rating_2,
-    }
-    details = {
-        'Ценовой диапазон': price_range,
-        'КУХНИ': cusines,
-        'Еда': meals,
-        'Рейтинги и отзывы': rating_dict
-    }
-    data_dict = {
-        'Название': name,
-        'Расписание': open_now,
-        'Детали': details,
-        'Адреса и ссылки': location_dict
-    }
-    info = {
-            'INFO': data_dict
-    }
-    data.append(info)
-
-    with open('items.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
-
-    time.sleep(3)
 
 
-except Exception as ex:
-    print(ex)
-finally:
-    driver.close()
-    driver.quit()
+def get_data():
+    driver = uc.Chrome(version_main=117, options=options)
+    data = []
+    with open('urls.txt') as file:
+        urls = file.readlines()
+    count = 0
+    try:
+        for url in urls:
+            driver.get(url)
+
+            try:
+                name = driver.find_element(By.CSS_SELECTOR, "[class='HjBfq']").text
+            except Exception as ex:
+                name = None
+
+            '''details'''
+            details = driver.find_elements(By.CSS_SELECTOR, "[class='SrqKb']")
+            try:
+                price_range = details[0].text
+            except IndexError:
+                price_range = None
+            try:
+                cusines = details[1].text
+            except IndexError:
+                cusines = None
+            try:
+                meals = details[2].text
+            except IndexError:
+                meals = None
+
+            '''details'''
+
+            '''media'''
+            photos_list = []
+            photos = driver.find_elements(By.CSS_SELECTOR, "[class='basicImg']")
+            if photos:
+                for photo in photos:
+                    photos_list.append(photo.get_attribute('src'))
+            else:
+                return None
+
+            ''''media'''
+
+            '''location and contacts'''
+            locs = driver.find_elements(By.CSS_SELECTOR, "[class='kDZhm IdiaP']")
+            address = locs[0].text
+
+            links = driver.find_elements(By.CSS_SELECTOR, "[class='YnKZo Ci Wc _S C FPPgD']")
+            try:
+                geo = links[0].get_attribute('href')
+            except IndexError:
+                geo = None
+            try:
+                website = links[1].get_attribute('href')
+            except IndexError:
+                website = None
+
+            phones = driver.find_elements(By.CSS_SELECTOR, "[class='BMQDV _F Gv wSSLS SwZTJ']")
+            try:
+                phone = phones[1].get_attribute('href')
+            except IndexError:
+                phone = None
+            '''location and contacts'''
+
+            '''Rating'''
+            try:
+                rating_num = driver.find_element(By.CSS_SELECTOR, "[class='ZDEqb']").text
+            except IndexError:
+                rating_num = None
+            try:
+                reviews_num = driver.find_element(By.CSS_SELECTOR, "[class='IcelI']").text
+            except IndexError:
+                reviews_num = None
+            try:
+                rating = driver.find_element(By.CSS_SELECTOR, "[class='cNFlb']").text
+            except IndexError:
+                rating = None
+            '''Rating'''
+            loc_links_dict = {
+                'Геолокация': geo,
+                'Вебсайт': website,
+                'Номер телефона': phone
+            }
+            location_dict = {
+                'Адрес': address,
+                'Способы связи': loc_links_dict
+            }
+            rating_dict = {
+                'Оценка': rating_num,
+                'Отзывы': reviews_num,
+                'Рейтинг': rating,
+            }
+            details = {
+                'Ценовой диапазон': price_range,
+                'КУХНИ': cusines,
+                'Еда': meals,
+                'Рейтинги и отзывы': rating_dict
+            }
+            data_dict = {
+                'Название': name,
+                'Медиа': photos_list,
+                'Детали': details,
+                'Адреса и ссылки': location_dict
+            }
+            info = {
+                'INFO': data_dict
+            }
+            data.append(info)
+
+            count += 1
+            print(f'Обработана {count}-я страница из {len(urls)}')
+        with open('items.json', 'a', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        time.sleep(10)
+    except Exception as ex:
+        print(ex)
+    finally:
+        driver.close()
+        driver.quit()
 
 
 if __name__ == '__main__':
-    parse_links()
+    get_data()
